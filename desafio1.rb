65219e001a1c2719f0b6c50118a0d3ec2813bc98
ENES DA SILVA MATOS <e.matos@pm.ba.gov.br>
2023-08-23 15:42:58 -0300

    desafio1

---
+++ desafio1.rb
@@ -0,0 +1,13 @@
+print "Digite seu nome: "
+
+nome = gets.chomp
+
+print "Digite seu sobrenome:   "
+
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+
+idade = gets.chomp
+
+puts "Seja bem vindo a nossa biblioteca, #{nome} #{sobrenome} classificaremos seus livros conforme sua #{idade}"
