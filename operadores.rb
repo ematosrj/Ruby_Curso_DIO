a7c15c54a6a12ff18ca3f86380dc8700538ce062
En Matos <e.matos_rj@hotmail.com>
2023-08-23 02:20:42 -0300

    adição operadores.rb

---
+++ operadores.rb
@@ -0,0 +1,26 @@
+irb(main):001:0> 12 + 13
+=> 25
+irb(main):002:0> num1 = 12 (variaveis)
+=> 12
+irb(main):003:0> num2 = 13 (variaveis)
+=> 13
+irb(main):004:0> num1 + num2
+=> 25
+irb(main):005:0> 50 - 30
+=> 20
+irb(main):006:0> num2 - num1
+=> 1
+irb(main):007:0> num1 - num2
+=> -1
+irb(main):008:0> 6 * 3
+=> 18
+irb(main):009:0> num1 + num1
+=> 24
+irb(main):010:0> 10/2
+=> 5
+irb(main):011:0> num2/num1 (na divisao só retorna inteiro)
+=> 1
+
+ => % modulo é para saber o resto da divisão 20/3 sobra 2
+
+ exponencial 5 elevado a 5 **
