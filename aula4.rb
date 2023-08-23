be0c0bb4db67d7946f0af048b2b281763c7b9f5a
En Matos <e.matos_rj@hotmail.com>
2023-08-23 02:04:15 -0300

    aula 3 e 4 add

--- aula4.rb
+++ aula4.rb
@@ -28,6 +28,21 @@ irb(main):002:0> a.class
 => String
 irb(main):003:0>
 
+#1)saída de dados na tela --> aparecer um comando na tela
+# usuário digite seu nome
+print "Digite seu nome:   "
+
+#2) programa permita que usuário preencha o que foi pedido
+# gets.chomp
+nome = gets.chomp
+
+print "Digite seu sobrenome:   "
+sobrenome = gets.chomp
+
+#3) saída final
+# para chamar um dado #{variável}
+puts "Oi #{nome} #{sobrenome} !"
+
 
 
 
