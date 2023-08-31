a5348f6f5b681b26aa6666ed81122c794e56ae52
ENES DA SILVA MATOS <e.matos@pm.ba.gov.br>
2023-08-31 13:52:19 -0300

    31/08/2023 as 13:52

---
+++ desafio2.rb
@@ -0,0 +1,33 @@
+#Desafio
+#Você receberá dois valores inteiros. Faça a leitura e em seguida calcule o produto entre estes dois valores. Atribua esta operação à variável PROD, mostrando esta de acordo com a mensagem de saída esperada (exemplo abaixo).   
+
+#Entrada
+#A entrada contém 2 valores inteiros.
+
+#Saída
+#Exiba a variável PROD conforme exemplo abaixo, tendo obrigatoriamente um espaço em branco antes e depois da igualdade.
+#Exemplos de Entrada	Exemplos de Saída
+#3
+#9 PROD = 27
+#-30 10 PROD = -300         
+#0 9 PROD = 0       
+
+#Desafios Ruby na DIO têm funções "gets" e "puts" acessíveis globalmente:
+
+# "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+
+# "puts": imprime um texto de saída (output), pulando linha.
+
+# ".to_i": Converte para int
+
+#saida de dados na tela --> aparecer um comando na tela.
+
+#usuario digite seu valor
+
+#programa permita que o usuario preencha o que foi pedido 
+
+#saida final 
+
+#firstValue = gets.to_i; secondValue = gets.to_i; PROD = firstValue * secondValue puts "PROD = #{PROD}"
+
+#TODO:  Crie as condições necessárias para calcular PROD
---
+++ times.rb
@@ -0,0 +1,23 @@
+#//ESTRUTURAS DE CONTROLE
+#TIMES
+#TIMES QUER DIZER VEZES VAI EXECUTAR UMA INSTRUÇÃO DETERMINADO NUMERO DE VEZES
+#a palavra "do" significa faça.
+
+#EXEMPLO 1 PRINT
+#6.times {print "Oi "}
+
+#EXEMPLO 1 PUTS
+#6.times {puts "Oi "}
+
+#EXEMPLO 2 PRINT
+#4.times do 
+#print "Oi"
+
+#EXEMPLO 2 PUTS
+#4.times do 
+#puts "Oi"
+
+#EXEMPLO 3 CONTADOR - neste exemplo cria um contador executando a variavel contador.
+20.times do |contador|
+puts "#{contador}"
+end
