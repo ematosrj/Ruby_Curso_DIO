0014d644c79f17483a70383f2ec754298c2c34c5
ENES DA SILVA MATOS <e.matos@pm.ba.gov.br>
2023-08-31 18:09:15 -0300

    31/08/2023 as 18:09

--- loop.rb
+++ loop.rb
@@ -1,2 +1,19 @@
 #DOWHILE - CONHECIDO COMO LOOPPING CRIA UM LAÇO DE REPETIÇÃO --FAZER ENQUANTO--- SÓ VAI PARAR QUANDO DER A INSTRUÇÃO BREAK.
 
+#loop execução
+count = 0
+
+loop do 
+    puts count
+#condição
+if count == 150
+
+break
+end
+
+#incremento -- > +1
+
+count += 5
+
+end
+
